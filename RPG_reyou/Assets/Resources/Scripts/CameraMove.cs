a7cc3fb0b3de4eaa5e4335769f1524a8e6b92b43
ryueunyoung <dmsdms95@gmail.com>
2016-09-28 17:50:40 +0900

    추가

---
+++ RPG_reyou/Assets/Resources/Scripts/CameraMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject player = null;
+    Vector3 oriPos = Vector3.zero;
+	// Use this for initialization
+	void Start ()
+    {
+        oriPos = this.transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.transform.position = oriPos + player.transform.position;
+	}
+}
---
+++ RPG_reyou/Assets/Resources/Scripts/PlayerAction.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAction : MonoBehaviour
+{
+    public enum state
+    {
+        사용안함 = -1,
+        이동 = 0,
+        점프
+    }
+    public float MoveSpeed = 3f;
+    Vector3 lookDirection;
+    public float jSpeed = 5f;
+    Rigidbody rigid;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) ||
+            Input.GetKey(KeyCode.RightArrow) ||
+            Input.GetKey(KeyCode.UpArrow) ||
+            Input.GetKey(KeyCode.DownArrow))
+        {
+            float xx = Input.GetAxisRaw("Vertical");
+            float zz = Input.GetAxisRaw("Horizontal");
+            lookDirection = xx * Vector3.forward + zz * Vector3.right;
+
+            this.transform.rotation = Quaternion.LookRotation(lookDirection);
+            this.transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+        }
+    }
+}
